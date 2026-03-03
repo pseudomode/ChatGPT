7a319b58a7acc0fb919144c7ed9120f817bad5f9
Matt McCulloch <matt@3dpox.com>
2026-03-03 15:29:53 -0800

    initial clone/add hh01 files

---
+++ hh01/data.js
@@ -0,0 +1,577 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360_aerial_island_8k",
+      "name": "360_aerial_island_8k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.010277452113600205,
+        "pitch": 0.1943643107122135,
+        "fov": 1.0841904824255306
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3998240160488713,
+          "pitch": 0.5948824629880978,
+          "rotation": 0,
+          "target": "10-ls_cine_droneappr_020001b"
+        },
+        {
+          "yaw": -0.061105007324934846,
+          "pitch": 0.3909033190287783,
+          "rotation": 0,
+          "target": "3-360_controlcenter02_8k"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.00394551565670298,
+          "pitch": 0.030915768966805146,
+          "title": "Center Island",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.70147357628327,
+          "pitch": -0.2685114323296425,
+          "title": "CBS",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.019807879651558835,
+          "pitch": -0.5857899644370228,
+          "title": "NBC",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.5846969007526255,
+          "pitch": -0.15340631622430934,
+          "title": "Water.org",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-360_approach03_8k",
+      "name": "360_Approach03_8k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.08173585597676158,
+        "pitch": 0.11438760205015441,
+        "fov": 0.9636759541736902
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.14494961194310108,
+          "pitch": 0.03247502972002181,
+          "rotation": 0,
+          "target": "2-360_patioentry_8k"
+        },
+        {
+          "yaw": -0.19723829020703398,
+          "pitch": 0.26103300855108635,
+          "rotation": 0,
+          "target": "6-360beach8k"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-360_patioentry_8k",
+      "name": "360_PatioEntry_8k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.3930390809036304,
+        "pitch": -0.18120457175928628,
+        "fov": 1.0841904824255306
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.034284051784512215,
+          "pitch": 0.045512329008769115,
+          "rotation": 0,
+          "target": "3-360_controlcenter02_8k"
+        },
+        {
+          "yaw": -0.8142785643561368,
+          "pitch": 0.04313805208411736,
+          "rotation": 0,
+          "target": "9-360pool8k"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-360_controlcenter02_8k",
+      "name": "360_controlCenter02_8k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 0.9636759541736902
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5292869894324959,
+          "pitch": -0.1817327668047497,
+          "rotation": 0,
+          "target": "0-360_aerial_island_8k"
+        },
+        {
+          "yaw": 1.4650793696243607,
+          "pitch": 0.002940407737279216,
+          "rotation": 0,
+          "target": "4-360_simportal_8k"
+        },
+        {
+          "yaw": -3.1269893147367505,
+          "pitch": 0.2142225569828824,
+          "rotation": 0,
+          "target": "2-360_patioentry_8k"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.4327166607937336,
+          "pitch": -0.09633508226525223,
+          "title": "Demo3",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.5234467626432764,
+          "pitch": -0.08941593764207667,
+          "title": "Demo2",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.5205118747149875,
+          "pitch": -0.2014141583271183,
+          "title": "Demo1",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.5278310045548942,
+          "pitch": -0.07997614475523562,
+          "title": "Demo4",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.4431128960230666,
+          "pitch": 0.24904858910241856,
+          "title": "TIOAT",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-360_simportal_8k",
+      "name": "360_simPortal_8k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 0.9636759541736902
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.026361074056147515,
+          "pitch": -0.19093746384417187,
+          "rotation": 0,
+          "target": "3-360_controlcenter02_8k"
+        },
+        {
+          "yaw": -0.021481068479122456,
+          "pitch": -0.07115239509045423,
+          "rotation": 0,
+          "target": "8-360kitchen8k"
+        },
+        {
+          "yaw": -0.01757631112035618,
+          "pitch": 0.044884754960106577,
+          "rotation": 0,
+          "target": "7-360bedroom8k"
+        },
+        {
+          "yaw": -0.01660002079704448,
+          "pitch": 0.152114805604743,
+          "rotation": 0,
+          "target": "5-360_centerisland_8k"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-360_centerisland_8k",
+      "name": "360_CenterIsland_8k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.05558924220559547,
+        "pitch": -0.08907928988159775,
+        "fov": 0.9636759541736902
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.021750025801139827,
+          "pitch": 0.06302038472638216,
+          "rotation": 0,
+          "target": "2-360_patioentry_8k"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-360beach8k",
+      "name": "360beach8k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.24308103199107656,
+        "pitch": -0.17592213499274223,
+        "fov": 0.9636759541736902
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.04383556620366846,
+          "pitch": -0.1273998955531468,
+          "rotation": 0,
+          "target": "2-360_patioentry_8k"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-360bedroom8k",
+      "name": "360bedroom8k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.5563272434697151,
+        "pitch": 0.09335483778233566,
+        "fov": 0.9636759541736902
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3707610459544082,
+          "pitch": 0.012733250564032517,
+          "rotation": 4.71238898038469,
+          "target": "4-360_simportal_8k"
+        },
+        {
+          "yaw": 1.5015626798032997,
+          "pitch": 0.03010724462406067,
+          "rotation": 1.5707963267948966,
+          "target": "8-360kitchen8k"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.12103186174013025,
+          "pitch": -0.2274442763998863,
+          "title": "Smoke Detector",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "8-360kitchen8k",
+      "name": "360kitchen8k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7348081269617435,
+          "pitch": 0.060718757283868996,
+          "rotation": 1.5707963267948966,
+          "target": "4-360_simportal_8k"
+        },
+        {
+          "yaw": -0.7685452461804942,
+          "pitch": 0.05110741326826407,
+          "rotation": 4.71238898038469,
+          "target": "4-360_simportal_8k"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.034519617029367566,
+          "pitch": 0.4859284687542349,
+          "title": "Kitchen Emergencies",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "9-360pool8k",
+      "name": "360pool8k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.003807263824196383,
+        "pitch": -0.24686977820744715,
+        "fov": 1.0841904824255306
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.29672727917197683,
+          "pitch": 0.008842250922466377,
+          "rotation": 4.71238898038469,
+          "target": "3-360_controlcenter02_8k"
+        },
+        {
+          "yaw": 0.45272280455031577,
+          "pitch": 0.031011745146621195,
+          "rotation": 0,
+          "target": "2-360_patioentry_8k"
+        },
+        {
+          "yaw": -1.633091860951735,
+          "pitch": 0.14668471364810998,
+          "rotation": 4.71238898038469,
+          "target": "5-360_centerisland_8k"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-ls_cine_droneappr_020001b",
+      "name": "LS_Cine_DroneAppr_02.0001b",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.28633193035317284,
+          "pitch": 0.5415862140694152,
+          "rotation": 0,
+          "target": "6-360beach8k"
+        },
+        {
+          "yaw": -0.12936328929223073,
+          "pitch": 0.16845033596233172,
+          "rotation": 0,
+          "target": "2-360_patioentry_8k"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "hero home",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
